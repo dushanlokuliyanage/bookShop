0e4708d34b893fc017968bbbdccba12aac0617e1
dushan loku liyanage <malingadushan27@gmail.com>
2025-10-13 13:29:23 +0530

    Git Uploded

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Sites</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+
+<body>
+    <!-- Header / Navbar -->
+    <header>
+        <nav class="navbar section-content">
+            <a href="#" class="nav-logo" style="text-decoration: none;">
+                <h2 class="logo-text">Books</h2>
+            </a>
+
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Feedback</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+
+        </nav>
+
+    </header>
+
+    <main>
+        <section class="hero-section">
+            <div class="section-content ">
+                <!-- Text Section -->
+                <div class="hero-details border">
+                    <h2 class="title">Best Books</h2>
+                    <h3 class="subtitle">Read your fav books here</h3>
+                    <p class="description">
+                        Quotes about books emphasize their ability to provide "portable magic" and serve as loyal companions and gateways to knowledge and new experiences.
+                        Key themes include books as portals to other worlds, sources of truth and understanding, and tools for personal growth and travel.
+                        Famous phrases like, "A room without books is like a body without a soul" (Cicero) and "There is no friend as loyal as a book"
+                        (Ernest Hemingway) capture the profound connection people have with books.
+                    </p>
+                    <div class="buttons">
+                        <a href="#" class="button order-now btn btn-outline-dark">Order Now</a>
+                        <a href="#" class="button contact-us btn btn-outline-dark">Contact Us</a>
+                    </div>
+                </div>
+
+                <!-- Image Section -->
+                <div class="hero-image-wrapper">
+                    <img src="imges/dark2x.png" alt="Hero" class="hero-image">
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
